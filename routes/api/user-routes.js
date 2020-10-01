be6e5fc25a22ea0d2b35783ec3596749727aa2bf
DJ Viking <anders.swedishviking@gmail.com>
2020-09-30 21:48:43 -0400

    craeted user controller methods

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,13 @@
 const router = require('express').Router();
 
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser
+} = require('../../controllers/user-controller.js');
+
 
 
 module.exports = router;
