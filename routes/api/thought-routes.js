039247a560e0109f794fb044b249ae37be9124dd
DJ Viking <anders.swedishviking@gmail.com>
2020-09-30 22:24:00 -0400

    added user and thought routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -7,4 +7,23 @@ const {
   deleteReaction
 } = require('../../controllers/thought-controller.js');
 
+// POST create a thought by userId
+// /api/thoughts/<userId>/
+router.route('/:userId')
+.post(addThought);
+
+//add a reaction
+//update a thought
+// /api/thoughts/<userId>/<thoughtId>/<reactionId>
+//delete a thought
+// /api/thoughts/<userId/<thoughtId>
+router.route('/:userId/:thoughtId')
+.put(addThought)
+.delete(deleteThought);
+
+//delete a reaction
+// /api/thoughts/<userId>/<thoughtId>/<reactionId>
+router.route('/:userId/:thoughtId/:reactionId')
+.delete(deleteReaction);
+
 module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -8,6 +8,19 @@ const {
   deleteUser
 } = require('../../controllers/user-controller.js');
 
+//user getAll and post routes
+// /api/users
+router
+.route('/')
+.get(getAllUsers)
+.post(createUser);
 
+//user get one, update one, and delete one
+// /api/users/<userId>
+router
+.route('/:id')
+.get(getUserById)
+.put(updateUser)
+.delete(deleteUser);
 
 module.exports = router;
