9ca723c5a33380c6229542e9a6de10d4a2aab1da
jogidipen <jogidipen@gmail.com>
2021-03-19 13:09:13 +0530

    Update server.js
    http added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const http = require('http');
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
